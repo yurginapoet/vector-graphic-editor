4fcf68eb1ee50225674f895b5718b3cabb37d76d
paraskun <ig.paraskun@gmail.com>
2026-02-21 07:52:26 +0000

    infra: #9: bootstrap server

---
+++ server/src/main/java/org/editor/Editor.java
@@ -0,0 +1,11 @@
+package org.editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Editor {
+  public static void main(String[] args) {
+    SpringApplication.run(Editor.class, args);
+  }
+}
